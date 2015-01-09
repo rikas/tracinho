6646a4625225578fc03c437f240bbfae84d28ce0
Ricardo Otero <oterosantos@gmail.com>
2015-01-09 00:29:01 +0000

    Finished world classifier

--- lib/tracinho/word_classifier.rb
+++ lib/tracinho/word_classifier.rb
@@ -7,30 +7,49 @@ module Tracinho
     end
 
     def full_classification
-      # case @word.text
-      # when /ste$/
-      #   "Segunda pessoa do singular do #{full_verb}."
-      # when /sse$/
-      #   "Primeira ou terceira pessoa do singular do #{full_verb}."
-      # when /mos$/
-      #   "Primeira pessoa do plural do presente do indicativo ou pretérito perfeito do indicativo do verbo #{verb}."
-      # when /#{DASH_CHAR}te$/
-      #   "Conjugação pronominal reflexa da segunda pessoa do singular do presente do indicativo do verbo #{verb}."
-      # when /#{DASH_CHAR}se$/
-      #   "Conjugação pronominal reflexa da segunda pessoa do singular do presente do indicativo do verbo #{verb}."
-      # when /#{DASH_CHAR}mos$/
-      #   "Conjugação pronominal da segunda pessoa do singular do imperativo do verbo #{verb}."
-      # end
+      case @word.text
+      when /ste$/
+        "Segunda pessoa do singular do #{full_verb}."
+      when /sse$/
+        "Primeira ou terceira pessoa do singular do #{full_verb}."
+      when /[^-]mos$/
+        "Primeira pessoa do plural do #{full_verb}."
+      when /(-te | -se)$/
+        "Conjugação pronominal reflexa da segunda pessoa do #{full_verb}."
+      when /-mos$/
+        "Conjugação pronominal da segunda pessoa do singular do #{full_verb}."
+      end
     end
 
     def verb
       endings = @word.text.match(ENDINGS).captures.compact
 
-      @word.text.sub(endings.first, 'r')
+      verb = @word.text.sub(endings.first, 'r')
+      verb.sub!(/rr$/, 'r')
+      verb.sub!(/our$/, 'ar')
+
+      # Irregular verbs that got bad guesses
+      verb = verb.sub(/^var$/, 'ir').sub(/^sor$/, 'ser').sub(/^for$/, 'ser')
+
+      verb
     end
 
     def verb_tense
+      case @word.text
+      when /ste$/
+        'pretérito perfeito do indicativo'
+      when /sse$/
+        'pretérito imperfeito do conjuntivo'
+      when /[^-]mos$/
+        'presente do indicativo ou pretérito perfeito do indicativo'
+      when /(-te | -se)$/
+        'presente do indicativo'
+      when /-mos$/
+        'imperativo'
+      end
     end
+
+    private
 
     def full_verb
       "#{verb_tense} do verbo #{verb}"
