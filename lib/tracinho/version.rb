9181c0dc07d6c77e132452fd820d9993caac95f6
Ricardo Otero <oterosantos@gmail.com>
2015-01-09 00:32:39 +0000

    New gem version

--- lib/tracinho/version.rb
+++ lib/tracinho/version.rb
@@ -1,5 +1,5 @@
 # This gem converts words with dash in the version without it and vice-versa. It also gets the verb
 # name from a word and gives the full grammar classification of the verb.
 module Tracinho
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
 end
